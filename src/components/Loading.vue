4fc46e481245f2ade67db16dd115c0e77bddc56c
zhaohong <2855675294@qq.com>
2020-08-11 19:02:53 +0800

    Upkeep

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -41,7 +41,8 @@ export default {
     top: 0;
     left: 0;
     right: 0;
-    bottom: 0;
+	bottom: 0;
+	z-index: 9999;
     background-color: rgba(0, 0, 0, 0.2);
 }
 
---
+++ src/views/Pages.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="about">
+    <Loading v-if="show"  />
+    <!-- 下拉框 -->
+     <Select v-model="model1" style="width:200px">
+        <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+    </Select>
+  </div>
+</template>
+
+<script>
+import Loading from '@/components/Loading.vue'
+
+export default {
+  name: "Pages",
+  components: {
+    Loading
+  },
+  data() {
+    return {
+      cityList: [
+                    {
+                        value: 'New York',
+                        label: 'New York'
+                    },
+                    {
+                        value: 'London',
+                        label: 'London'
+                    },
+                    {
+                        value: 'Sydney',
+                        label: 'Sydney'
+                    },
+                    {
+                        value: 'Ottawa',
+                        label: 'Ottawa'
+                    },
+                    {
+                        value: 'Paris',
+                        label: 'Paris'
+                    },
+                    {
+                        value: 'Canberra',
+                        label: 'Canberra'
+                    }
+                ],
+                model1: '',
+      show:true
+      
+    };
+  },
+  methods: {},
+  mounted() {
+    console.log("ok")
+
+    console.log(this.$route.query);
+    setTimeout(()=>{
+    this.show = false
+
+    },2000)
+
+  },
+  created(){
+  }
+};
+</script>
+<style scoped>
+
+</style>
