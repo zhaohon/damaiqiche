fe22b4dda5bf23fcbb3ad103dcac60e3bc2a1349
zhaohong <2855675294@qq.com>
2020-08-13 17:18:35 +0800

    17:18

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,25 @@
       <router-link to="/about">About</router-link> |
       <router-link to="/pages">Pages</router-link>
     </div>
-    <Public />
+    <Head />
     <!-- 顶部占位 -->
     <div style="width:100%;height:116px;"></div>
     <router-view/>
     <!-- 底部占位 -->
-    <div style="width:100%;height:210px;"></div>
+    <Foot />
   </div>
 </template>
 <script>
 // @ is an alias to /src
-import Public from '@/components/public.vue'
+import Head from '@/components/head.vue'
+import Foot from '@/components/foot.vue'
+
 
 export default {
-  name: 'public',
+  name: 'App',
   components: {
-    Public
+    Head,
+    Foot
   },
   data () {
     return {
---
+++ src/components/head.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="public">
+	  
+	  <!-- 顶部 -->
+	  <div class="heander-box">
+		  <div class="heander fbox fbox-ac fbox-jb">
+				<a href="" title="大唛养车-汽车保养回归工业化" >
+					<img src="../assets/top-logo.png" alt="大唛养车">
+				</a>
+				<h2>为我的爱车约保养</h2>
+			</div>
+	  </div>
+
+
+	  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "public",
+  props: [],
+  methods: {},
+};
+</script>
+
+
+<style scoped>
+	.heander-box{width: 100%;position: fixed;top: 0;left: 0;background-color: #FFFFFF;height: 116px;border-bottom: 1px solid #F5F6FA;box-sizing: border-box;}
+	.heander{width: 1200px;margin: 0 auto;height: 100%;}
+	.heander h2{font-size: 36px;color: #333333;}
+@media only screen and (max-width: 1200px) {
+  .heander{width: 100%;}
+}
+@media only screen and (max-width: 800px) {
+	.heander{justify-content: center !important;}
+  .heander h2{display: none;}
+}
+@media only screen and (max-width: 500px) {
+	.heander a{width: 90%;margin: 0 auto;}
+	.heander img{width: 100%;}
+}
+</style>
